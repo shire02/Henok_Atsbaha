06b58263d7dfbf2f68be83092cc7f1009bfd085a
Henok Atsbaha <henokats@gmail.com>
2017-12-26 11:44:15 -0500

    routing

--- personal_web/App_Start/RouteConfig.cs
+++ personal_web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace personal_web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "per", id = UrlParameter.Optional }
             );
         }
     }
